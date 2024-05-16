5397a78659dc9f0362dd34fd6f6815544afa90a5
Unflojo21 <132239050+Unflojo21@users.noreply.github.com>
2024-05-16 08:31:52 -0400

    Add files via upload

--- sensor.ino
+++ sensor.ino
@@ -17,7 +17,7 @@ void setup()
   pinMode(azul, OUTPUT);
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
-
+  int seg = 0;
 	// Clears the LCD screen
 	lcd.clear();
   Serial.begin(9600);
@@ -36,7 +36,79 @@ void loop()
   duration = pulseIn(echo, HIGH);
   distance = duration / 59;  // Dividido por 59 para convertir microsegundos en centímetros
   
+  if(distance < 3000){
+    long distance2 = duration/59;
+    delay(275);
+    if(distance+5 > distance2 && distance-5 < distance2){
+      if(distance < 20){
+        delay(250);
+        long distance3 = duration/59;
+        if(distance+5 > distance3 && distance-5 < distance3){
+        digitalWrite(rojo, 102);
+        digitalWrite(verde, LOW);
+        digitalWrite(azul, LOW);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Choca");
+        lcd.setCursor(0,1);
+        lcd.print("Distancia:");
+        lcd.setCursor(10,1);
+        lcd.print(distance);
+        lcd.setCursor(14,1);
+        lcd.print("cm");
+        }
 
+      }
+      else if (distance < 90){
+        digitalWrite(rojo, LOW);
+        digitalWrite(verde, 51);
+        digitalWrite(azul, LOW);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Permitido");
+        if(distance > 70){
+          lcd.setCursor(10,0);
+          lcd.print("CAR");
+        }
+        else if(distance > 50){
+          lcd.setCursor(10,0);
+          lcd.print("MTRUK");
+        }
+        else{
+          lcd.setCursor(10,0);
+          lcd.print("TRUK");
+        }
+        lcd.setCursor(0,1);
+        lcd.print("Distancia:");
+        lcd.setCursor(10,1);
+        lcd.print(distance);
+        lcd.setCursor(14,1);
+        lcd.print("cm");
+      }
+      else{
+        digitalWrite(rojo,0);
+        digitalWrite(verde,0);
+        digitalWrite(azul,1);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Asfalto");
+        lcd.setCursor(0,1);
+        lcd.print("Distancia:");
+        lcd.setCursor(10,1);
+        lcd.print(distance);
+        lcd.setCursor(14,1);
+        lcd.print("cm");
+      }
+    }
+  }
+
+
+
+
+
+
+
+  /*/
   if (distance < 50) {
     digitalWrite(rojo, HIGH);
     digitalWrite(verde, LOW);
@@ -83,6 +155,6 @@ void loop()
     lcd.print("cm");
   }
 
-  
+  /*/
   delay(50);  // Retardo opcional para reducir la velocidad de actualización del sensor
 }
